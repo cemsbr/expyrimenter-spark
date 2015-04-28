649bc2262451303744a6b9ded4dca64255dce89e
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2015-04-29 05:32:09 +0900

    History Server, refactoring

--- expyrimenter/plugins/spark/spark.py
+++ expyrimenter/plugins/spark/spark.py
@@ -12,17 +12,12 @@ class Spark:
         self._home = home
         self._master = master
         self.executor = executor
-        self.slaves = slaves
         self.logger_name = 'spark'
+        self._slaves = slaves
 
-    @property
-    def slaves(self):
-        return self._slaves
-
-    @slaves.setter
-    def set_slaves(self, value):
-        if value is not None:
-            self._slaves = value
+    def set_slaves(self, hosts):
+        self._slaves = hosts
+        if hosts:
             self._write_slaves_file()
 
     def start(self):
@@ -31,33 +26,59 @@ class Spark:
 
     def stop(self):
         cmd = self._home + '/sbin/stop-all.sh'
-        self._ssh_master(cmd, 'start')
+        self._ssh_master(cmd, 'stop')
 
-    def submit(self, filename):
-        cmd = 'spark-submit ' + quote(filename)
-        title = 'run ' + filename
+    def start_history_server(self):
+        self._history_server('start')
+
+    def stop_history_server(self):
+        self._history_server('stop')
+
+    def submit(self, filename, file_stdout, file_stderr):
+        cmd = '{}/bin/spark-submit {} 1>{} 2>{}'.format(self._home,
+                                                        quote(filename),
+                                                        quote(file_stdout),
+                                                        quote(file_stderr))
+        title = 'app ' + filename
+        ssh = SSH(self._master, cmd,
+                  title=title,
+                  stdout=True,
+                  logger_name=self.logger_name)
+        self.executor.run(ssh)
+
+    def clean_tmp(self):
+        """Remove temp files."""
+        self._ssh_hosts('rm -rf /tmp/spark*-*-*-*-*-*', 'clean')
+
+    def clean_logs(self, hosts=None):
+        """Remove log and temp files."""
+        paths = ('/logs/*', '/work/*')
+        full_paths = ' '.join((self._home + p for p in paths))
+        cmd = 'rm -rf /tmp/spark*-*-*-*-*-* ' + full_paths
+        title = 'clean logs -'
+        self._ssh_hosts(cmd, title, hosts)
+
+    def _history_server(self, action):
+        cmd = '{}/sbin/{}-history-server.sh'.format(self._home, action)
+        title = action + ' history server'
         self._ssh_master(cmd, title)
 
-    def clean(self):
-        # TODO anything to clean?
-        pass
-
-    def purge(self):
-        """Be careful: this will erase all logs. You must stop() before."""
-        cmd = 'rm -rf {}/logs/* {}/work/*'.format(self._home, self._home)
-        title = 'erase logs'
-        self._ssh_all(cmd, title)
-
-    def _write_slaves_file(self, spark_home):
-        filename = quote(spark_home + '/conf/slaves')
+    def _write_slaves_file(self):
+        filename = quote(self._home + '/conf/slaves')
         cmd = '>{}; for slave in {}; do echo $slave >>{}; done'.format(
-            filename, ' '.join(self.slaves), filename)
-        self._ssh_master(cmd, 'slaves file')
+            filename, ' '.join(self._slaves), filename)
+        self._ssh_master(cmd, 'slaves file', True)
 
-    def _ssh_master(self, cmd, title):
+    def _ssh_master(self, cmd, title, stdout=False):
         ssh = SSH(self._master, cmd, title=title, logger_name=self.logger_name)
+        self.executor.run(ssh)
 
-    def _ssh_all(self, cmd, title):
-        for host in self.slaves + [self._master]:
-            ssh = SSH(host, cmd, title=title, logger_name=self.logger_name)
+    def _ssh_hosts(self, cmd, title, hosts=None):
+        if hosts is None:
+            hosts = [self._master] + self._slaves
+        for host in hosts:
+            host_title = '{} {}'.format(title, host)
+            ssh = SSH(host, cmd,
+                      title=host_title,
+                      logger_name=self.logger_name)
             self.executor.run(ssh)
